208b252f3f0595a32f70bfe206861c2f4a20050d
Алексей <@KURTZPC>
2016-04-26 10:50:20 +0300

    Добавьте файлы проекта.

---
+++ PG.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Common
+{
+    public class Class1
+    {
+    }
+}
